c260d20a069af773f0464210fa5786f3d70f413a
Sourav-03 <sourav060898@gmail.com>
2022-07-17 23:19:36 +0530

    Initial Commit

---
+++ lib/full_screen_network_image.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library full_screen_network_image;
+
+export 'src/full_screen_network_image_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ lib/src/full_screen_network_image_base.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+/// Checks if you are awesome. Spoiler: you are.
+class FullScreenNetworkImage extends StatefulWidget {
+  final ImageFrameBuilder? frameBuilder;
+
+  final ImageLoadingBuilder? loadingBuilder;
+
+  final ImageErrorWidgetBuilder? errorBuilder;
+
+  final double? width;
+
+  final double? height;
+
+  final Color? color;
+
+  final Animation<double>? opacity;
+
+  final FilterQuality filterQuality;
+
+  final BlendMode? colorBlendMode;
+
+  final BoxFit? fit;
+
+  final AlignmentGeometry alignment;
+
+  final ImageRepeat repeat;
+
+  final Rect? centerSlice;
+
+  final bool matchTextDirection;
+
+  final bool gaplessPlayback;
+
+  final String? semanticLabel;
+
+  final bool excludeFromSemantics;
+
+  final bool isAntiAlias;
+
+  final String src;
+
+  const FullScreenNetworkImage(
+    this.src, {
+    Key? key,
+    double scale = 1.0,
+    this.frameBuilder,
+    this.loadingBuilder,
+    this.errorBuilder,
+    this.semanticLabel,
+    this.excludeFromSemantics = false,
+    this.width,
+    this.height,
+    this.color,
+    this.opacity,
+    this.colorBlendMode,
+    this.fit,
+    this.alignment = Alignment.center,
+    this.repeat = ImageRepeat.noRepeat,
+    this.centerSlice,
+    this.matchTextDirection = false,
+    this.gaplessPlayback = false,
+    this.filterQuality = FilterQuality.low,
+    this.isAntiAlias = false,
+    Map<String, String>? headers,
+    int? cacheWidth,
+    int? cacheHeight,
+  })  : assert(cacheWidth == null || cacheWidth > 0),
+        assert(cacheHeight == null || cacheHeight > 0),
+        super(key: key);
+
+  @override
+  FullScreenNetworkImageState createState() => FullScreenNetworkImageState();
+}
+
+class FullScreenNetworkImageState extends State<FullScreenNetworkImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.network(
+        //PREDEFINE ATTRIBUTE VALUES
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+        //USER DEFINE ATTRIBUTE VALUES
+        widget.src,
+        frameBuilder: widget.frameBuilder,
+        loadingBuilder: widget.loadingBuilder,
+        errorBuilder: widget.errorBuilder,
+        color: widget.color,
+        opacity: widget.opacity,
+        filterQuality: widget.filterQuality,
+        colorBlendMode: widget.colorBlendMode,
+        repeat: widget.repeat,
+        centerSlice: widget.centerSlice,
+        matchTextDirection: widget.matchTextDirection,
+        gaplessPlayback: widget.gaplessPlayback,
+        semanticLabel: widget.semanticLabel,
+        excludeFromSemantics: widget.excludeFromSemantics,
+        isAntiAlias: widget.isAntiAlias,
+      ),
+    );
+  }
+}
